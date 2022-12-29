35ab9e7d6a17051a9a3317b48945ee3a6c07b7ab
hanier10 <hsmp16@gmail.com>
2022-12-29 00:52:39 -0600

    transformando string a number con pipes

--- src/mobiles/mobiles.controller.ts
+++ src/mobiles/mobiles.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { MobilesService } from './mobiles.service';
 
 @Controller('mobiles')
@@ -11,7 +11,7 @@ export class MobilesController {
   }
 
   @Get('/:id')
-  getPhonesById(@Param('id') id: string) {
-    return this.mobilesService.findOneById(+id);
+  getPhonesById(@Param('id', ParseIntPipe) id: number) {
+    return this.mobilesService.findOneById(id);
   }
 }
