61720fd9378925b8a1b3ef988a1e059f448c8ebd
hanier10 <hsmp16@gmail.com>
2023-01-07 23:35:07 -0600

    add new resource called seed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MobilesModule } from './mobiles/mobiles.module';
 import { BrandsModule } from './brands/brands.module';
+import { SeedModule } from './seed/seed.module';
 
 @Module({
-  imports: [MobilesModule, BrandsModule],
+  imports: [MobilesModule, BrandsModule, SeedModule],
   controllers: [],
   providers: [],
   exports: [],
